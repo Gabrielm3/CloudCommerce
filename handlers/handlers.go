58c0b4cbbb9baf1ef6b8af7ad06326bb3360258a
Gabriel Mendes <contatoeugm@gmail.com>
2024-05-14 04:51:14 -0300

    feat: Add SelectProduct function for retrieving product data

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -77,6 +77,8 @@ func ProcProducts(body string, path string, method string, user string, id int, 
 		return routers.UpdateProduct(body, user, id)
 	case "DELETE":
 		return routers.DeleteProduct(user, id)
+	case "GET":
+		return routers.SelectProduct(request)
 	}
 
 	return 400, "Method invalid"
