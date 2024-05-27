ed62d76ff7dc95fdb702366bdd1be6f4c05aecd1
Gabriel Mendes <contatoeugm@gmail.com>
2024-05-27 14:02:00 -0300

    feat: Add Logo component to Layout

---
+++ client/src/components/Layout/Logo/Logo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { Image } from "semantic-ui-react";
+
+export function Logo() {
+  return (
+    <Link href="/">
+        <h2>logo</h2>
+        <Image src="" alt="" />
+    </Link>
+  )
+}
