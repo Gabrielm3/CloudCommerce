7ac4a390eb0b1fd34fa7cfd465414e25bfec8bc6
Gabriel Mendes <contatoeugm@gmail.com>
2024-05-27 03:39:21 -0300

    feat: Add constants and amplify configuration for AWS Cognito authentication

---
+++ client/src/utils/constants.js
@@ -0,0 +1,16 @@
+export const ENV = {
+    API_URL: "http://localhost:8080",
+    MEDIA_API: "http://localhost:8080/media",
+    MEDIA_URL: "http://localhost:8080/media/url",
+    ENDPOINTS: {
+        USERS: "users",
+        USER_ME: "user/me",
+        ADDRESS: "address",
+        CATEGORY: "category",
+        PRODUCT: "product",
+        ORDER: "order",
+        ORDERS: "orders",
+    },
+    TOKEN: "token",
+    BASKET: "basket",
+};
