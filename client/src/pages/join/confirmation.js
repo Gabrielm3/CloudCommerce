d96c158391b6a410e2dc3aca129be539b591de9c
Gabriel Mendes <contatoeugm@gmail.com>
2024-05-28 16:12:45 -0300

    feat: Add Separator component to Shared folder

---
+++ client/src/pages/join/confirmation.js
@@ -0,0 +1,9 @@
+import { JoinLayout } from "@/layouts";
+import { ConfirmationForm } from "@/components/Auth";
+
+export default function ConfirmationPage() {
+  return;
+  <JoinLayout>
+    <ConfirmationForm />
+  </JoinLayout>;
+}
